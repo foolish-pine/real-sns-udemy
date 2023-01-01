101e284c57e5854be39ffe6a4f2d2f7600dd5726
foolish-pine <matsuda.takuya41@gmail.com>
2023-01-01 15:01:45 +0900

    cp24

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,110 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+// ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body,
+			});
+			res.status(200).json(user);
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else {
+		return res.status(403).json("自身のアカウントの情報のみ更新できます。");
+	}
+});
+
+// ユーザー情報の削除
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findByIdAndDelete(req.params.id);
+			res.status(200).json(user);
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else {
+		return res.status(403).json("自身のアカウントの情報のみ削除できます。");
+	}
+});
+
+// ユーザー情報の取得
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		// eslint-disable-next-line no-unused-vars
+		const { password, updatedAt, ...others } = user._doc;
+		res.status(200).json(others);
+	} catch (error) {
+		return res.status(500).json(error);
+	}
+});
+
+// ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+
+			if (!currentUser.followings.includes(req.params.id)) {
+				await user.updateOne({
+					$push: {
+						followers: req.body.userId,
+					},
+				});
+				await currentUser.updateOne({
+					$push: {
+						followings: req.params.id,
+					},
+				});
+				return res.status(200).json("フォローに成功しました。");
+			} else {
+				return res
+					.status(403)
+					.json("あなたは既にこのユーザーをフォローしています。");
+			}
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else {
+		return res.status(500).json("自身はフォローできません。");
+	}
+});
+
+// ユーザーのフォローをはずす
+router.put("/:id/unfollow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+
+			if (currentUser.followings.includes(req.params.id)) {
+				await user.updateOne({
+					$pull: {
+						followers: req.body.userId,
+					},
+				});
+				await currentUser.updateOne({
+					$pull: {
+						followings: req.params.id,
+					},
+				});
+				return res.status(200).json("フォロー解除しました。");
+			} else {
+				return res
+					.status(403)
+					.json("あなたはこのユーザーをフォローしていません。");
+			}
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else {
+		return res.status(500).json("自身はフォロー解除できません。");
+	}
+});
 
 module.exports = router;
