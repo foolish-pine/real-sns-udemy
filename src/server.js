ba341307f1a2da693f7782299e479baca66e6985
foolish-pine <matsuda.takuya41@gmail.com>
2023-01-01 13:38:40 +0900

    cp18

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const userRoute = require("./routes/users");
 const PORT = 3000;
 
+require("dotenv").config();
+
+// データベース接続
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => {
+		console.log("DBと接続中・・・");
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
 // ミドルウェア
+app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
